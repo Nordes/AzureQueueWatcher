c6e3430d5c21d1b59578aa5a72c90b51551e30a1
Nordes Ménard-Lamarre <nordes@gmail.com>
2016-11-20 00:41:12 +0100

    Added some message when starting and removed the "process.exit"

--- program.js
+++ program.js
@@ -7,15 +7,18 @@ nconf.env().file({ file: 'config.json', search: true, format: require('hjson') }
 const jobSettings = nconf.get('jobSettings');
 const watchSettings = nconf.get('watchSettings');
 
+console.log('Starting...');
 const aqw = azureQueueWatcher(jobSettings);
 
 if (!nconf.get('clean')) {
+  console.log('-> Initializing all the watch')
   watchSettings.forEach(function (settings, index, allSettings) {
     aqw.start(settings);
   });
+
+  console.log('-> All ready to go...');
 }
 else {
+  console.log('-> Cleaning the logs')
   aqw.clean();
 }
-
-process.exit;
