e9a8a0615c3522b99c2f177d81e4046275d1de53
Nordes Ménard-Lamarre <nordes@gmail.com>
2016-11-20 22:10:28 +0100

    Update the templates

--- program.js
+++ program.js
@@ -1,5 +1,4 @@
 'use strict'
-
 const azureQueueWatcher = require('./modules/azureQueueWatcher');
 const nconf = require('nconf');
 
@@ -10,15 +9,26 @@ const watchSettings = nconf.get('watchSettings');
 console.log('Starting...');
 const aqw = azureQueueWatcher(jobSettings);
 
-if (!nconf.get('clean')) {
+if (nconf.get('clean')) {
+  console.log('-> Cleaning the logs')
+  aqw.clean();
+} else if (nconf.get('install-svc')) {
+  // Todo: look to install as a service : http://stackoverflow.com/questions/10547974/how-to-install-node-js-as-windows-service
+  switch (nconf.get('install-svc')) {
+    case 'windows':
+      throw new Error("Not implemented");
+    // break;
+    case 'linux':
+      throw new Error("Not implemented");
+    // break;
+    default:
+      throw new Error("Unsupported configuration");
+  }
+} else {
   console.log('-> Initializing all the watch')
   watchSettings.forEach(function (settings, index, allSettings) {
     aqw.start(settings);
   });
 
   console.log('-> All ready to go...');
-}
-else {
-  console.log('-> Cleaning the logs')
-  aqw.clean();
 }
